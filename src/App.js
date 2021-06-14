2e6c97239d3c465161a1d5bbc8668b79684c29df
Max Conrad <maxconrad@me.com>
2021-06-13 20:48:42 -0400

    Remove some boilerplate from app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss'
+import HookCalculator from './hooks/HookCalculator'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        React Calculator
       </header>
+      <div className="body">
+        <p>
+          Redux
+        </p>
+        <p>
+          Context & Hooks
+        </p>
+        <HookCalculator />
+      </div>
     </div>
   );
 }
